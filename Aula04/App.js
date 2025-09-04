3b4fbd9a25515e1d042ab0e133b5b195d320c8f5
lucasroque1 <126733101+lucasroque1@users.noreply.github.com>
2025-09-04 23:14:15 +0000

    (04/09)Atividade Aula 04

---
+++ Aula04/App.js
@@ -0,0 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './screens/DetailsScreen';
+import PerfilScreen from './screens/PerfilScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Início" component={HomeScreen} />
+        <Stack.Screen name="Detalhes" component={DetailsScreen} />
+        <Stack.Screen name="Perfil" component={PerfilScreen} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
