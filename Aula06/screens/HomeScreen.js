a8faa2ae2cff8ce38277bd1de3d47b5b7c07ccb0
lucasroque1 <126733101+lucasroque1@users.noreply.github.com>
2025-10-09 23:37:21 +0000

    Aula06 09/10

---
+++ Aula06/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Tela Inicial</Text>
+      <Button
+        title="Exemplo Básico"
+        onPress={() => navigation.navigate('Exemplo Básico')}
+      />
+      <Button
+        title="Validação de Formulário"
+        onPress={() => navigation.navigate('Validação de Formulário')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  titulo: { fontSize: 24, fontWeight: 'bold' }
+});
